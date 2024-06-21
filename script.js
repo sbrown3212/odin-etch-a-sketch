9b6b8fc174004f3deb44c49433cbd560f6cad222
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-06-21 15:43:17 -0600

    Add color change functionality on square hover

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ function createGrid(squaresPerSide) {
         let square = document.createElement('DIV');
         let flexBasis = 1/squaresPerSide*100;
 
-        square.style.border = '1px dotted red';         // for visualization purposes
+        square.className = 'square';
+        square.style.border = '1px solid lightgray';         // for visualization purposes
         square.style.flexBasis = `${flexBasis}%`;
         container.appendChild(square);
     }
 }
 
 createGrid(16);
+
+container.addEventListener('mouseover', (e) => {
+    if (e.target.className === 'square') {
+        e.target.style.backgroundColor = 'black';
+    }
+});
